9e1b5bd1fa6df360dd3f3328d5be5be0f786fc99
Nicole Kinsella <nkinsell@udel.edu>
2023-09-01 15:15:37 -0400

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Edited - Nicole Kinsella.
+                Hello World -- Edit <code>src/App.tsx</code> and save. This page
+                will automatically reload. Edited - Nicole Kinsella.
             </p>
         </div>
     );
