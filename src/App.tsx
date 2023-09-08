fc554ada7b83ca094117bb8914b4e0bd2ee4bb8e
Nicole Kinsella <nkinsell@udel.edu>
2023-09-08 14:58:20 -0400

    Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { Button, Col, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "./App.css";
+import IMG_4040 from "../src/images/IMG_4040.jpg";
 
 function App(): JSX.Element {
     return (
@@ -7,10 +10,42 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>Header 1</h1>
+            <img src={IMG_4040} alt="A picture of my cat" />
+            <ol>
+                <li>1.1</li>
+                <li>1.2</li>
+                <li>1.3</li>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </ol>
             <p>
                 Hello World -- Edit <code>src/App.tsx</code> and save. This page
                 will automatically reload. Edited - Nicole Kinsella.
             </p>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                height: 50,
+                                width: 100,
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                height: 50,
+                                width: 100,
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
