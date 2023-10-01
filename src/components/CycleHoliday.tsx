a6ec50b4056171ad49081ec8fd69f64f4deafeb2
Nicole Kinsella <nkinsell@udel.edu>
2023-10-01 17:31:27 -0400

    Completed Components

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,41 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    type holidayEmoji = "🎄" | "🐰" | "🎆" | "🍀" | "🦃";
+    const Alphabet_Transitions: Record<holidayEmoji, holidayEmoji> = {
+        "🎄": "🐰",
+        "🐰": "🎆",
+        "🎆": "🍀",
+        "🍀": "🦃",
+        "🦃": "🎄"
+    };
+    const Year_Transitions: Record<holidayEmoji, holidayEmoji> = {
+        "🍀": "🐰",
+        "🐰": "🎆",
+        "🎆": "🦃",
+        "🦃": "🎄",
+        "🎄": "🍀"
+    };
+    const [holiday, setHoliday] = useState<holidayEmoji>("🎄");
+    function changeAlphabetically(): void {
+        const newHoliday = Alphabet_Transitions[holiday];
+        setHoliday(newHoliday);
+    }
+    function changeByYear(): void {
+        const newHoliday = Year_Transitions[holiday];
+        setHoliday(newHoliday);
+    }
+    return (
+        <div>
+            Holiday: {holiday}
+            <div>
+                <Button onClick={changeAlphabetically}>
+                    Advance by Alphabet
+                </Button>
+            </div>
+            <div>
+                <Button onClick={changeByYear}>Advance by Year</Button>
+            </div>
+        </div>
+    );
 }
