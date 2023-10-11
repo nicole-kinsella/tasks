1f9791d0a06c8a1e5294bbb49a46cea6568c4fa8
Nicole Kinsella <nkinsell@udel.edu>
2023-10-11 15:19:46 -0400

    Completed CheckAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+function checkIfCorrect(userAnswer: string, expectedAnswer: string) {
+    if (userAnswer === expectedAnswer) {
+        return "✔️";
+    } else {
+        return "❌";
+    }
+}
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="formCheckAnswer">
+                <Form.Label>Input Answer:</Form.Label>
+                <Form.Control
+                    as="textarea"
+                    value={userAnswer}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setUserAnswer(event.target.value)
+                    }
+                />
+            </Form.Group>
+            <div>{checkIfCorrect(userAnswer, expectedAnswer)}</div>
         </div>
     );
 }
