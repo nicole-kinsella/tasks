ffe83286fc3fc2d106423358905972c28d439de3
Nicole Kinsella <nkinsell@udel.edu>
2023-10-12 12:03:06 -0400

    Completed MultipleChoiceQuestion and Task 10

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,31 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selected, setSelected] = useState<string>(options[0]);
+    function updateSelected(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelected(event.target.value);
+    }
+    function checkIfCorrect(userAnswer: string, expectedAnswer: string) {
+        if (userAnswer === expectedAnswer) {
+            return "✔️";
+        } else {
+            return "❌";
+        }
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="favoriteColors">
+                <Form.Label>Choose the Correct Answer</Form.Label>
+                <Form.Select value={selected} onChange={updateSelected}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{checkIfCorrect(selected, expectedAnswer)}</div>
         </div>
     );
 }
